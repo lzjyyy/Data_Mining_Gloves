6592457e7df530b30c550c7900e363e7d165faab
hzz-zzh <zhouzhdrew@gmail.com>
2026-05-20 17:08:02 +0800

    first commit, this is template project with freeRTOS

---
+++ MDK-ARM/RTE/_glovesV0.1/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'glovesV0.1' 
+ * Target:  'glovesV0.1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
